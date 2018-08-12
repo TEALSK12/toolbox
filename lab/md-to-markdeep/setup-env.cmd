930ef3f416d70ff96a3be0f54256e4887bb5673c
Steve Hollasch <steve@hollasch.net>
2018-08-12 14:36:28 -0700

    2deep: Update setup-env to locate curriculum src

--- lab/md-to-markdeep/setup-env.cmd
+++ lab/md-to-markdeep/setup-env.cmd
@@ -1,8 +1,32 @@
 @echo off
 
+REM -- Add the md-to-markdeep/tools director to the execution path.
+
+path | findstr >nul -i md-to-markdeep/tools
+if %errorlevel% equ 0 goto :setupCurriculumDir
+echo Setting up path.
 if not exist tools (
     echo 1>&2Error: Run this command while in the md-to-markdeep directory.
     exit /b 1
 )
 
 path %cd%\tools;%path%
+
+REM -- Aet the APCSA_CURRICULUM environment variable to point to the APCSA curriculum source.
+
+:setupCurriculumDir
+if defined APCSA_CURRICULUM goto :foundCurriculum
+
+if exist ..\..\..\apcsa-instructor\apcsa-public goto :foundCurriculumDirectory
+echo 1>&2Error: APCSA_CURRICULUM is not defined, and apcsa-public not found via expected path.
+exit /b 1
+
+:foundCurriculumDirectory
+pushd ..\..\..\apcsa-instructor\apcsa-public
+    set APCSA_CURRICULUM=%CD%
+popd
+
+:foundCurriculum
+echo APCSA_CURRICULUM = %APCSA_CURRICULUM%
+
+exit /b 0
